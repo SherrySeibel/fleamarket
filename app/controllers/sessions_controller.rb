6c9046efdd2f193303e02876cf7e32a441b056ef
Sherry Seibel <sherry.seibel@gmail.com>
2014-07-28 14:37:00 -0400

    Update syntax for user_params and session_params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,11 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:email, :password)
+    params.require(:session).
+      permit(
+        :email,
+        :password,
+    )
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,11 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).
+      permit(
+        :email,
+        :password,
+    )
   end
 end
 
