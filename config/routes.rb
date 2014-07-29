f30c8b037e3f5cb5e323bc20af9c3a835f4c6bf8
Casey Gruppioni <cgruppioni@gmail.com>
2014-07-29 15:37:33 -0400

    move user show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ require "monban/constraints/signed_in"
 
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create]
 
   constraints Monban::Constraints::SignedIn.new do
     resources :categories, only: [:index, :create]
     resources :products, only: [:new, :create, :show, :index]
+    resources :users, only: [:show]
 
     get "/", to: "dashboards#show", as: :dashboard
   end
