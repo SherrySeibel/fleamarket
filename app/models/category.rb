19960ba1acf0dabfc021b4b058aa18f94e9d5548
Andrew Slawter <aslawter@gmail.com>
2014-07-29 10:47:33 -0400

    Display categories alphabetically

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   has_many :products, dependent: :destroy
+
+  def self.alphabetical
+    order(:name)
+  end
 end
