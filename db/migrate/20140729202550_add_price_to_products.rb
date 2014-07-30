e691afdbdebcd7216023d489727a46e27787e3ab
Casey Gruppioni <cgruppioni@gmail.com>
2014-07-30 10:00:11 -0400

    change product migration to null false

--- db/migrate/20140729202550_add_price_to_products.rb
+++ db/migrate/20140729202550_add_price_to_products.rb
@@ -1,6 +1,6 @@
 class AddPriceToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :starting_price, :decimal, null: true, default: 0
+    add_column :products, :starting_price, :decimal, null: false, default: 0
 
     change_column_null :products, :starting_price, false
   end
