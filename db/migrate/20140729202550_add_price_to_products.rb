240168d72999e1600c3a7acafdf593df678e88ee
Casey Gruppioni <cgruppioni@gmail.com>
2014-07-30 10:13:50 -0400

    delete null field

--- db/migrate/20140729202550_add_price_to_products.rb
+++ db/migrate/20140729202550_add_price_to_products.rb
@@ -1,7 +1,5 @@
 class AddPriceToProducts < ActiveRecord::Migration
   def change
     add_column :products, :starting_price, :decimal, null: false, default: 0
-
-    change_column_null :products, :starting_price, false
   end
 end
