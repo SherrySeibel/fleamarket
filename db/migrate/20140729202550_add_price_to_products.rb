0a6ffdc4b27e709f7ee0ba0b9d47f90289d85858
Casey Gruppioni <cgruppioni@gmail.com>
2014-07-30 09:51:49 -0400

    change product price migration

--- db/migrate/20140729202550_add_price_to_products.rb
+++ db/migrate/20140729202550_add_price_to_products.rb
@@ -1,8 +1,6 @@
 class AddPriceToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :starting_price, :float, null: true
-
-    update("UPDATE products SET starting_price = 0.00")
+    add_column :products, :starting_price, :decimal, null: true, default: 0
 
     change_column_null :products, :starting_price, false
   end
