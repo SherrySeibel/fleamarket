08c5cb05f5b48c1ff2f51ca5543796548630bdbe
Casey Gruppioni <cgruppioni@gmail.com>
2014-08-01 14:57:30 -0400

    change @sellers

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,7 @@ class PaymentsController < ApplicationController
   def new
     @payment = Payment.new
     @products = Product.all
-    @sellers = User.where.not(id: current_user.id)
+    @sellers = User.sellers
   end
 
   def create
@@ -12,7 +12,7 @@ class PaymentsController < ApplicationController
       redirect_to root_path
     else
       @products = Product.all
-      @sellers = User.where.not(id: current_user.id)
+      @sellers = User.sellers
       render :new
     end
   end
